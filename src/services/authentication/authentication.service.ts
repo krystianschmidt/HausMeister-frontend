a2aa82174f1baf1977e507149bb67a2c072afd2b
Krystian Schmidt <krystian.schmidt@siemens.com>
2024-08-16 16:52:42 +0200

    set up login and some structural changes

--- src/services/authentication/authentication.service.ts
+++ src/services/authentication/authentication.service.ts
@@ -1,44 +1,53 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {IAmService} from "./i-am.service";
-import {RegisterUser} from "../../app/models/registerUser";
-
+import { HttpClient, HttpHeaders } from "@angular/common/http"; // Importiert HttpClient und HttpHeaders für HTTP-Anfragen
+import { IAmService } from "./i-am.service"; // Importiert einen anderen Service, der für HTTP-Anfragen verwendet wird
+import { RegisterUser } from "../../app/models/registerUser"; // Importiert das RegisterUser-Modell
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // Der Service wird auf Root-Ebene bereitgestellt
 })
-export class AuthenticationService{
+export class AuthenticationService {
+  // Definiert den Pfad zum Backend-Endpunkt für Authentifizierung
   private readonly backendUrlPath: string = 'auth';
 
-
+  // Konstruktor injiziert den IAmService, der für HTTP-Anfragen verwendet wird
   constructor(private readonly iAmService: IAmService) {
   }
 
-
+  // Methode zum Einloggen des Benutzers
   login(credentials?: any) {
+    // Setzt die HTTP-Header für die Basic-Authentifizierung
     const headers = new HttpHeaders(credentials ?
-      {authorization: 'Basic ' + btoa(credentials.username + ':' + credentials.password)} :
-      {authorization: ''});
+      { authorization: 'Basic ' + btoa(credentials.username + ':' + credentials.password) } :
+      { authorization: '' });
 
+    // Verwendet den IAmService, um den Benutzer zu laden
     return this.iAmService.loadUser(headers);
   }
 
-
-  logout(){
+  // Methode zum Ausloggen des Benutzers
+  logout() {
+    // Sendet eine POST-Anfrage zum Logout-Endpunkt und setzt den Benutzer zurück
     return this.iAmService.sendPostRequest(this.backendUrlPath + '/logout', {}).then(() => {
-      this.iAmService.iAmUser = {username:"", name: ""};
+      this.iAmService.iAmUser = { username: "", name: "" };
     });
   }
 
-  register(user: RegisterUser){
+  // Methode zum Registrieren eines neuen Benutzers
+  register(user: RegisterUser) {
+    // Sendet eine POST-Anfrage zum Register-Endpunkt mit den Benutzerdaten
     return this.iAmService.sendPostRequest(this.backendUrlPath + '/register', user);
   }
 
-  isUsernameAvailable(username: string){
-    return this.iAmService.sendGetRequest(this.backendUrlPath + `/available`, {username: username})
+  // Methode zur Überprüfung, ob ein Benutzername verfügbar ist
+  isUsernameAvailable(username: string) {
+    // Sendet eine GET-Anfrage zum Available-Endpunkt mit dem Benutzernamen
+    return this.iAmService.sendGetRequest(this.backendUrlPath + `/available`, { username: username });
   }
 
+  // Methode zur Überprüfung, ob der Benutzer authentifiziert ist
   isAuthenticated(): boolean {
+    // Überprüft, ob der Benutzername im IAmService gesetzt ist
     return !!this.iAmService.iAmUser.username;
   }
 }
--- src/services/authentication/i-am.service.ts
+++ src/services/authentication/i-am.service.ts
@@ -1,35 +1,38 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpParams} from "@angular/common/http";
-import { iAmUser } from 'src/app/models/iAmUser';
-import {CommunicationRequestService} from "../lib/communication-request.service";
+import { HttpClient, HttpParams } from "@angular/common/http"; // Importiert HttpClient und HttpParams für HTTP-Anfragen
+import { iAmUser } from 'src/app/models/iAmUser'; // Importiert das iAmUser-Modell
+import { CommunicationRequestService } from "../lib/communication-request.service"; // Importiert den CommunicationRequestService
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // Der Service wird auf Root-Ebene bereitgestellt
 })
-export class IAmService extends CommunicationRequestService<any>{
+export class IAmService extends CommunicationRequestService<any> {
 
+  // Initialisiert einen Benutzer mit leeren Feldern
   iAmUser: iAmUser = {
     name: "", username: ""
   };
 
+  // Definiert den Pfad zum Backend-Endpunkt für Authentifizierung
   protected readonly backendUrlExt: string = 'auth';
 
-
+  // Bereitet die HTTP-Parameter für die Anfrage vor
   protected prepareRequestObjectParameter(reqParameter: any): HttpParams {
-    if(reqParameter.username)
+    // Wenn der Parameter einen Benutzernamen enthält, setzt diesen in den HttpParams
+    if (reqParameter.username)
       return new HttpParams().set('username', reqParameter.username);
 
-    return new HttpParams();
+    return new HttpParams(); // Andernfalls gibt leere HttpParams zurück
   }
 
-  loadUser(headers?: any){
+  // Lädt den Benutzer anhand der übergebenen Header
+  loadUser(headers?: any) {
+    // Verwendet die Methode sendGetRequest des CommunicationRequestService, um eine GET-Anfrage zu senden
     return super.sendGetRequest<iAmUser>(this.backendUrlExt + '/iAm', null, headers)
       .then((user: any) => {
+        // Setzt den erhaltenen Benutzer in das iAmUser-Objekt und gibt den Benutzer zurück
         this.iAmUser = user
         return user;
       });
   }
-
-
-
 }
